8953a21f97792b1105760dc9a4e7f791a3f8c2b8
Training_C2A.04.29 <Training_C2A.04.29@accenture.com>
2019-09-10 14:29:48 +0530

    Fundamentals

---
+++ src/test/java/prodlin/runner.java
@@ -0,0 +1,12 @@
+package prodlin;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(monochrome=true,plugin={"json:report.json"})
+public class runner {
+
+}
